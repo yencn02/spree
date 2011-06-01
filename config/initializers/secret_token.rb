7c9c75abf6157695021fb39ce17617e6685f6e3c
yenhtb <yenhtb@ubuntu-taint5.(none)>
2011-06-01 13:06:15 +0700

    Initialize spree app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spree::Application.config.secret_token = 'c38bdbd753ef9748fc607c87b622e37a8c19eaadcba8c405ce0b374bc220c97a297f303470b1dadbeec4abdea34306f6a99831faaec25989d773126f9dbde66d'
